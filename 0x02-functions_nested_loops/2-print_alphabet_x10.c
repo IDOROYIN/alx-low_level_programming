be314a6610b73ad055910d52ca78d9c2ef16166f
IDOROYIN <vpaul6832@gmail.com>
2023-09-13 10:41:51 -0700

    task 2 print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - prints alphabet in lowercase 10x
+ * Return: 0;
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'a' ; j++)
+			_putchar(j);
+	}
+	_putchar('\n');
+}
